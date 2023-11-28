496edba1b3284a35da5c15889de4ecd72cd234f6
Enes Çevik <enes.cevik@ciceksepeti.com>
2023-11-28 14:28:21 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cs_demo_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
